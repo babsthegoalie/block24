ab1cb058a9a123926e743d4ed02dbd9d97422989
babsthegoalie <bzkondracki@gmail.com>
2023-09-25 17:19:01 -0600

    puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {puppyList} from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
 
-  console.log(puppyList)
+  console.log(puppies)
 
   return (
     <>
+{ 
 
+puppies.map((puppy) => {
+  return <p>{puppy.name}</p>
+})
+
+
+}
 
     </>
   )

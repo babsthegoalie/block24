1ad773d52fe4b16a421c440f2026f3d2a6b9fa59
babsthegoalie <bzkondracki@gmail.com>
2023-09-25 18:12:11 -0600

    css

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,42 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
 
   console.log(puppies)
+  {()=>{console.log("puppy id: ", puppy.id)}}
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
     <>
 { 
 
 puppies.map((puppy) => {
-  return <p>{puppy.name}</p>
+  return <p className="pupName" onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
 })
 
 
 }
+{ featPupId && <p className="featPupId">{featPupId}</p> }
+
+{featPupId && (
+        <div>
+          <h2 className="featPupName">{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
+
 
     </>
   )
+
+
 }
 
 
